6383e97bc35b7df86612049da10ce42fc360935d
Jonathan Doughty <jwd630@gmail.com>
2024-03-09 17:07:54 -0500

    login_actions doc & cleanup

--- login_actions.sh
+++ login_actions.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # Actions to set up standard applications on login
 
-# This gets symlinked as app_script in a Platypus app which runs as a Login item
+# This gets symlinked as app_script in a Platypus https://sveinbjorn.org/platypus
+# app which I run as a Login item
 
+# I create a 40GB case sensitive sparsebundle where most of git repos
+# and other work lives. See https://stackoverflow.com/a/12085233/1124740
 sparsebundle="${HOME}/CM.sparsebundle"
-sparse_volume="/Volumes/CM"
 sourcedir="${HOME}/CM/dotfiles"  # where the master copy of this lives
 app_script="${HOME}/Applications/LogInScript.app/Contents/Resources/script"
 verbose=1
@@ -12,32 +14,35 @@ log=
 [ $verbose -gt 1 ] && log=~/login_actions.txt
 
 adjust_path () {
+    # Consider adding a file instead to /etc/paths.d/
     PATH="/opt/homebrew/bin:/usr/local/bin:${PATH}"
 }
 
 mount_cm () {
+    # Check that my case sensitive sparsebundle, generally a separate login item, is accessible
     declare -i times delay=2
 
-    # Check that my case sensitive sparsebundle, generally a separate login item, is accessible
     if [ ! -d "$sourcedir" ]; then
         if [ -d "$sparsebundle" ]; then
-            [ $verbose -gt 0 ] && printf "Opening %s\n" "$sparsebundle"
-            open "$sparsebundle"    # Get macos to mount the sparsebundle as a volume
+            [ $verbose -gt 0 ] && \
+                printf "Opening %s\n" "$sparsebundle"
+            open "$sparsebundle"    # Get macOS to mount the sparsebundle as a volume
             sleep $delay
         else
             printf "NO %s ?!!\n" "$sparsebundle"
             exit 1
         fi
     else
-        [ $verbose -gt 0 ] && printf "%s is accessible\n" "$sourcedir"
+        [ $verbose -gt 0 ] && \
+            printf "%s is accessible\n" "$sourcedir"
         return 0
     fi
 
     # Wait for sparsebundle contents to appear
-
     for times in 10 9 8 7 6 5 4 3 2 1 0; do
         if [ ! -d "$sourcedir" ]; then
-            [ $verbose -gt 1 ] && printf "Waiting %s seconds for %s\n" $((times * delay)) "$sourcedir"
+            [ $verbose -gt 1 ] && \
+                printf "Waiting %s seconds for %s\n" $((times * delay)) "$sourcedir"
             sleep $delay
         else
             break
@@ -50,21 +55,9 @@ mount_cm () {
     return 0
 }
 
-dismount_cm () {
-    local pids
-    declare -i p
-    # Signal processes (other than this one) accessing sparse bundle
-    pids=$(lsof "$sparse_volume" | awk 'NR>1 {print $2}' | grep -v $$ | sort -u)
-    for p in $pids; do
-        kill "$p"
-    done
-    diskutil eject "$sparse_volume"
-}
-
 check_scripts_sources () {
-    # Let me know (in iTerm where I can do something about it) if the CM'd version of this and
+    # Let me know (in terminal where I can do something about it) if the CM'd version of this and
     # related scripts are out of sync.
-    #trap "set +x" RETURN && set -x # fun^ction debugging
     if [ -n "$TERM_PROGRAM" ]; then
         cmp "$sourcedir/${BASH_SOURCE[0]##*/}" "${BASH_SOURCE[0]}"
         cmp "$sourcedir/Justfile" "Justfile"
@@ -83,15 +76,22 @@ ssh_identities () {
     # (cd ~/.ssh; ssh-add --apple-use-keychain [private key files])
 
     if [ -S "$SSH_AUTH_SOCK" ]; then
-        [ $verbose -gt 1 ] && printf "ssh-agent processes:\n%s\n" "$(pgrep -l ssh-agent)" && \
+        [ $verbose -gt 1 ] && \
+            printf "ssh-agent processes:\n%s\n" "$(pgrep -l ssh-agent)" && \
             printf "SSH_AUTH_SOCK's owner: %s\n" "$(lsof -t "$SSH_AUTH_SOCK")"
         if ! /usr/bin/ssh-add -l >/dev/null 2>&1; then
-            # Sierra+ enables adding identities with passphrases stored in Keychain via ssh-add -K
-            # ssh-add will add all local keys with Keychain passwords
-            (builtin cd ~/.ssh || exit ; /usr/bin/ssh-add -q --apple-load-keychain --apple-use-keychain)
-            [ $verbose -gt 0 ] && printf "Added the following identities to ssh agent %s\n" "$SSH_AUTH_SOCK"
+            # Sierra+ enables adding identities with passphrases stored in Keychain via
+            # ssh-add --apple-use-keychain
+            # ssh-add --apple-load-keychaion will add all local keys with Keychain passwords
+            (                   # In a subshell
+                builtin cd ~/.ssh || exit
+                /usr/bin/ssh-add -q --apple-load-keychain --apple-use-keychain
+            )
+            [ $verbose -gt 0 ] && \
+                printf "The following identities are available to ssh agent %s\n" "$SSH_AUTH_SOCK"
         else
-            [ $verbose -gt 0 ] && printf "ssh identities already added\n"
+            [ $verbose -gt 0 ] && \
+                printf "ssh identities already added\n"
         fi
         ssh-add -l | awk '{printf "\t%s\n", $3}'
     else
@@ -101,10 +101,20 @@ ssh_identities () {
 
 start_hammerspoon () {
     # Make sure my primary Mac crutch is in place
-    if ! pgrep -q Hammerspoon ; then
-        open -a Hammerspoon && [ $verbose -gt 0 ] && printf "Started Hammerspoon\n"
+    local crutch=Hammerspoon
+    if ! pgrep -q "$crutch" ; then
+        local id app
+        # Look for the application
+        id=$(osascript -e "id of application \"$crutch\"" 2>/dev/null)
+        app="$(osascript -e "tell application \"Finder\" to POSIX path of (get application file id \"$id\" as alias)")"
+        if [[ -n "$app" ]]; then
+            open -a "$app" && [ $verbose -gt 0 ] && \
+                printf "Started %s\n" "$crutch"
+        else
+            printf "%s not found\n" "$crutch"
+        fi
     else
-        [ $verbose -gt 0 ] && printf "Hammerspoon already started\n"
+        [ $verbose -gt 0 ] && printf "%s already started\n" "$crutch"
     fi
 }
 
@@ -169,7 +179,7 @@ login_actions () {
                 printf "Unrecognized %s option: %s\n" "$0" "$1"
                 ;;
         esac
-    else                        # auto sequence
+    else                        # auto sequence through all functions
         adjust_path
         mount_cm || exit 1      # or all further bets are off
         check_scripts_sources
@@ -179,7 +189,7 @@ login_actions () {
     fi
 }
 
-
+# Allow for command line testing of individual functions
 [ $verbose -gt 1 ] && set -x
 case "$0" in
     (*login*|*script*)
