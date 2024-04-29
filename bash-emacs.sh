0b02e5c4e432b1aa87de075a90b91436c2325e8d
Jonathan Doughty <jwd630@gmail.com>
2024-04-29 11:08:01 -0400

    clean up emacs integration

--- bash-emacs.sh
+++ bash-emacs.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # bash-emacs - helpers for command line interaction with *the* editor
 
+# Guesses at path for emacsclient unless EMACSCLIENT_PATH has been set
+
 if [ -n "$ZSH_VERSION" ]; then emulate -L ksh; fi
 
 function emc {
-    # As suggested by http://emacsformacosx.com/tips
+    # As suggested by https://www.emacswiki.org/emacs/EmacsClient#h5o-28
+    # back in the day, specify a non-standard socket location
     local SOC="/tmp/emacs_socket_for_${LOGNAME}/server"
+    # Note that this also requires
+    # (setq server-socket-dir (format "/tmp/emacs_socket_for_%s" (user-login-name)))
+    # prior to (server-start)
     local CLIENT
 
     if [ ! -e "$SOC" ]; then
@@ -19,24 +25,31 @@ function emc {
         esac
     fi
 
-    # Places where emacslient lives; yakshave as a bash array / loop
-    if [ -e ~/Applications/Emacs.app/Contents/MacOS/bin/emacsclient ]; then
-        CLIENT=~/Applications/Emacs.app/Contents/MacOS/bin/emacsclient # My local application version
-    elif [ -e /Applications/Emacs.app/Contents/MacOS/bin/emacsclient ]; then
-        CLIENT=/Applications/Emacs.app/Contents/MacOS/bin/emacsclient # YAMAMOTO's native Mac version
-    elif [ -e /usr/local/bin/emacsclient ]; then
-        CLIENT=/usr/local/bin/emacsclient # Something I've installed
-    elif [ -e /usr/bin/emacsclient ]; then
-        CLIENT=/usr/bin/emacsclient # OS's emacs
-    elif [ -e /snap/bin/emacsclient ]; then
-        CLIENT=/snap/bin/emacsclient  # Ubuntu via snap
+    # Places where emacslient has been known to live
+    declare -a paths
+    if [[ -n "$EMACSCLIENT_PATH" && -e "$EMACSCLIENT_PATH" ]]; then
+        # Add user specified path as first in array
+        paths+=("$EMACSCLIENT_PATH")
     fi
+    paths+=("/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
+    paths+=("${HOME}/Applications/Emacs.app/Contents/MacOS/bin/emacsclient")
+    paths+=("${HOME}/Applications/EmacsMacPort.app/Contents/MacOS/bin/emacsclient")
+    paths+=("/usr/local/bin/emacsclient")
+    paths+=("/usr/bin/emacsclient")
+    paths+=(/"snap/bin/emacsclient")  # Ubuntu via snap
+    local c
+    for c in "${paths[@]}"; do
+        if [[ -e "$c" ]]; then
+            CLIENT="$c"
+            break
+        fi
+    done
 
     if [ -n "$CLIENT" ]; then
         if [ ! -e "$SOC" ]; then
             while ! "$CLIENT" -q -s "$SOC" -e t 2>/dev/null ; do
                 printf "Waiting for server-start\n"
-                sleep 2
+                sleep 1
             done
         fi
         # shellcheck disable=SC2068 # I want to send each array element
