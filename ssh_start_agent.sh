4a53cf982783ece86e3a835d7103b79c48830798
Jonathan Doughty <jwd630@gmail.com>
2024-05-13 16:44:31 -0400

    bash version dependency; DRYer directory name

--- ssh_start_agent.sh
+++ ssh_start_agent.sh
@@ -3,9 +3,21 @@
 
 # Inspired by https://stackoverflow.com/questions/18880024/start-ssh-agent-on-login
 ssh_start_agent () {
-    # shellcheck disable=SC2046 # word splitting is wanted
-    unset $(echo -n "$(env | grep -o '^SSH_[^=]*')")  # start clean
-    SSH_ENV="${HOME}/.ssh/environment"; export SSH_ENV
+    if [[ ${BASHVERSINFO[1]} -lt 5 ]]; then
+        # shellcheck disable=SC2046 # start clean construct for bash 4 unset issue
+        eval $(echo unset $(echo -n $(env | grep -o '^SSH_[^=]*' | tr '\n' ' ')))
+    else
+        # shellcheck disable=SC2046 # works for bash and zsh
+        unset $(env | grep -o '^SSH_[^=]*' | tr '\n' ' ') # start clean
+    fi
+    local ssh_dir="${HOME}/.ssh"
+
+    if [[ ! -e "$ssh_dir" ]]; then
+        mkdir -p "$ssh_dir"
+        chmod 700 "$ssh_dir"
+        printf "Created %s; replace as needed\n" "$ssh_dir"
+    fi
+    SSH_ENV="${ssh_dir}/environment"; export SSH_ENV
     # shellcheck source=/dev/null
     [ -e "${SSH_ENV}" ] && . "${SSH_ENV}"
     ps -fp "${SSH_AGENT_PID:-1}" | grep -q ssh-agent$ || {
@@ -15,12 +27,13 @@ ssh_start_agent () {
     case "${OSTYPE}" in
         (linux*)
             if ssh-add -l | grep -q 'no identities' ; then
-                find ~/.ssh -name 'id*' -print0 | xargs -0 ssh-add # all local identities; not just defaults
+                 # add all local identities; not just defaults
+                find "$ssh_dir" -name 'id*' -print0 | xargs -0 ssh-add
             fi
             ;;
         (darwin*)
-            # Add keys added to Keychain, e.g., via ssh-add -K
-            (builtin cd ~/.ssh || exit; ssh-add -q --apple-load-keychain 2>/dev/null )
+            # Add keys added to Keychain
+            (builtin cd "$ssh_dir" || exit; ssh-add -q --apple-load-keychain 2>/dev/null )
             ;;
         (*)
             echo "No ssh_agent on ${OSTYPE^?}?"
