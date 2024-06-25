c0de3d43219b99ce6fffb6fe065499dd39e8d3ca
Jonathan Doughty <jwd630@gmail.com>
2024-06-24 21:02:42 -0400

    discover emacs' server socket rather than keep a defined one in sync

--- bash-emacs.sh
+++ bash-emacs.sh
@@ -6,16 +6,24 @@
 if [ -n "$ZSH_VERSION" ]; then emulate -L ksh; fi
 
 function emc {
-    # As suggested by https://www.emacswiki.org/emacs/EmacsClient#h5o-28
-    # back in the day, specify a non-standard socket location
-    local SOC="/tmp/emacs_socket_for_${LOGNAME}/server"
-    # Note that this also requires
-    # (setq server-socket-dir (format "/tmp/emacs_socket_for_%s" (user-login-name)))
-    # prior to (server-start)
+
+    function _find_emacs_socket () {
+        case ${OSTYPE} in
+            (darwin*)
+                lsof -c Emacs | grep 'server$' | awk '{print $(NF) }'
+                ;;
+            (linux*)
+                lsof -c emacs | grep 'server$' | awk '{print $(NF) }' # untested
+                ;;
+        esac
+    }
+    
+    local SOC
+    SOC=$(_find_emacs_socket)
     local CLIENT
 
     if [ ! -e "$SOC" ]; then
-        case ${OSTYPE} in # though there is no no current dependence on OS version
+        case ${OSTYPE} in 
             (darwin*)
                 osascript -e 'tell application "Emacs" to activate'
                 ;;
@@ -45,13 +53,23 @@ function emc {
         fi
     done
 
+    #trap "set +x" RETURN && set -x
     if [ -n "$CLIENT" ]; then
-        if [ ! -e "$SOC" ]; then
-            while ! "$CLIENT" -q -s "$SOC" -e t 2>/dev/null ; do
-                printf "Waiting for server-start\n"
+        while [[ ! -e "$SOC" ]]; do
+            # Look for whatever socket Emacs has open
+            SOC=$(_find_emacs_socket)
+            if [[ -z "$SOC" ]]; then
+                printf "Waiting for Emacs to create server socket\n"
                 sleep 1
-            done
-        fi
+            else
+                break
+            fi
+        done
+        
+        while ! "$CLIENT" -q -s "$SOC" -e t &>/dev/null ; do
+            printf "Waiting for server-start\n"
+            sleep 1
+        done
         # shellcheck disable=SC2068 # I want to send each array element
         "$CLIENT" -s "$SOC" -n -a \"\" $@
     else
