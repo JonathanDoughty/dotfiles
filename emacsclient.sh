e31306586b8d00191b414f3f1ed7691309e0a417
Jonathan Doughty <jwd630@gmail.com>
2024-09-19 15:20:28 -0400

    deal with emacsclient within emacs' shell

--- emacsclient.sh
+++ emacsclient.sh
@@ -1,23 +1,90 @@
 #!/usr/bin/env bash
 # bash-emacs - helpers for command line interaction with *the* editor
 
-# Guesses at path for emacsclient unless EMACSCLIENT_PATH has been set
-
 function emc {
+    # This assumes that the user's init sequence includes (server-start)
+    local SOC # path to Emacs' server socket
 
-    function _find_emacs_socket () {
+    function _emc_start_emacs () {
+        # If server's socket can't be found then start Emacs
+        SOC=$(_emc_find_emacs_socket)
+        if [ ! -e "$SOC" ]; then
+            case ${OSTYPE} in
+                (darwin*)
+                    osascript -e "tell application \"Emacs\" to activate"
+                    ;;
+                (linux*)
+                    emacs --daemon
+                    ;;
+            esac
+        fi
+    }
+
+    function _emc_find_emacs_socket () {
+        # Find the socket that Emacs server will have started
+        SOC=""
         case ${OSTYPE} in
             (darwin*)
                 # $APP won't work for EmacsForMacOS
                 # Sigh: we have to ignore errors that might come from mounted Time Machine volumes
-                lsof -w -c Emacs | grep 'server$' | awk '{print $(NF) }'
+                SOC=$(lsof -w -c Emacs | grep 'server$' | awk '{print $(NF) }')
                 ;;
             (linux*)
-                lsof -c emacs | grep 'server$' | awk '{print $(NF) }' # untested
+                SOC=$(lsof -c emacs | grep 'server$' | awk '{print $(NF) }') # untested
                 ;;
         esac
+        printf "%s" "$SOC"
     }
-    
+
+    function _emc_wait_for_server_response () {
+        declare -i TIMER=1
+        while [[ ! -e "$SOC" ]]; do
+            # Look for whatever socket Emacs has open
+            SOC=$(_emc_find_emacs_socket)
+            if [[ -z "$SOC" ]]; then
+                printf "Waiting %d seconds for Emacs to create server socket\n" "$TIMER"
+                sleep $TIMER
+                (( TIMER+=1 ))
+            else
+                break
+            fi
+        done
+
+        # Insure client gets response from server
+        TIMER=1
+        while ! "$CLIENT" -q -s "$SOC" -e t &>/dev/null ; do
+            printf "Waiting %d seconds for server response\n" "$TIMER"
+            sleep $TIMER
+            (( TIMER+=1 ))
+        done
+    }
+
+    function _emc_find_client () {
+        # Find the emacsclient associated with the running Emacs
+        local EPATH="" process client=""
+        case ${OSTYPE} in
+            (darwin*)
+                process="Emacs"
+                ;;
+            (linux*)
+                process="emacs"
+                ;;
+        esac
+        # Within Emacs shell, on macOS, pgrep doesn't find the Emacs process
+        # Possibly https://unix.stackexchange.com/a/100759/13887 ?
+        #EPATH=$(ps -fp $(pgrep "$process") | tail -1 | awk '{print $NF}')
+        # alternately, unchecked on Linux
+        #EPATH=$(killall -s "$process") | awk '{print $NF}')
+        # shellcheck disable=SC2009
+        EPATH=$(ps aux | grep $process | grep -v grep | awk '{print $NF}')
+        # Traverse filesystem hierarchy looking for client
+        while [[ "${EPATH%/*}" != "" && "$client" == "" ]]; do
+            client="$(find "${EPATH%/*}" -name emacsclient)"
+            EPATH="${EPATH%/*}"
+        done
+        echo "$client"
+    }
+
     local -i VERBOSE
     OPTIND=1                    # needed for subsequent function getopts
     while getopts "v" flag "$@"; do
@@ -30,68 +97,15 @@ function emc {
     if [[ "${VERBOSE}" -gt 0 && -z "$ZSH_VERSION" ]]; then
         set -x && trap "set +x" RETURN EXIT
     fi
-    
-    # Find emacslient and emacs app in places where they has been seen
-    declare -a paths
-    if [[ -n "$EMACSCLIENT_PATH" && -e "$EMACSCLIENT_PATH" ]]; then
-        # Add user specified path as first in array
-        paths+=("$EMACSCLIENT_PATH")
-    fi
-    paths+=("${HOME}/Applications/EmacsForMacOS.app/Contents/MacOS/bin/emacsclient");
-    paths+=("${HOME}/Applications/Emacs.app/Contents/MacOS/bin/emacsclient");
-    paths+=("/Applications/Emacs.app/Contents/MacOS/bin/emacsclient");
-    paths+=(/"snap/bin/emacsclient")  # Ubuntu via snap
-    paths+=("/usr/local/bin/emacsclient")
-    paths+=("/usr/bin/emacsclient")
 
-    # Which client and app? Order above matters.
-    local CLIENT APP c
-    for c in "${paths[@]}"; do
-        if [[ -e "$c" ]]; then
-            CLIENT="$c"
-            if [[ "${CLIENT%%.app*}" != "${CLIENT}" ]]; then
-                # Extract app name from client path
-                APP="${CLIENT%%.app*}"
-                APP="${APP##*/}"
-                [[ "$VERBOSE" -gt 0 ]] && \
-                    printf "Using app %s\nand client %s\n" "$APP" "$CLIENT"
-            fi
-            break
-        fi
-    done
+    _emc_start_emacs
 
-    local SOC
-    SOC=$(_find_emacs_socket)
-    if [ ! -e "$SOC" ]; then
-        case ${OSTYPE} in 
-            (darwin*)
-                osascript -e "tell application \"$APP\" to activate"
-                ;;
-            (linux*)
-                emacs --daemon
-                ;;
-        esac
-    fi
-
-    declare -i TIMER=1
+    local CLIENT
+    CLIENT=$(_emc_find_client)
     if [ -n "$CLIENT" ]; then
-        while [[ ! -e "$SOC" ]]; do
-            # Look for whatever socket Emacs has open
-            SOC=$(_find_emacs_socket)
-            if [[ -z "$SOC" ]]; then
-                printf "Waiting %d seconds for Emacs to create server socket\n" "$TIMER"
-                sleep $TIMER
-                (( TIMER+=1 ))
-            else
-                break
-            fi
-        done
-        
-        while ! "$CLIENT" -q -s "$SOC" -e t &>/dev/null ; do
-            printf "Waiting for server-start\n"
-            sleep 1
-        done
-        # shellcheck disable=SC2068 # I want to send each array element
+        _emc_wait_for_server_response
+
+        # shellcheck disable=SC2068 # Send each array element as separate argument
         "$CLIENT" -s "$SOC" -n -a \"\" $@
     else
         printf "Can't find emacsclient to open %s\n" "$@"
